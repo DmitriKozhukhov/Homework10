f11559dbf8c9dc91f5f3c7401be77e022a73dbe1
Dmitri Kozhukhov <dmitrikozhukhov@gmail.com>
2023-12-19 21:28:20 +0300

    task44 solution

--- robots.py
+++ robots.py
@@ -5,6 +5,19 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI1': lst})
+rbt = []
+hmn = []
+for item in lst:
+    if item == 'robot':
+        rbt.append(1)
+        hmn.append(0)
+    else:
+        rbt.append(0)
+        hmn.append(1)
+data['robot'] = rbt
+data['human'] = hmn
+
+print(data)
 
 
 
